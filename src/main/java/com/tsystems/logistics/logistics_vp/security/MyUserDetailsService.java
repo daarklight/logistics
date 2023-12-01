847a335d43ffc0a5a2f4d6ab96e1cb805e37c745
daarklight <56450811+daarklight@users.noreply.github.com>
2023-12-01 23:45:58 +0100

    Logistics - v11 - second branch with security

---
+++ src/main/java/com/tsystems/logistics/logistics_vp/security/MyUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.tsystems.logistics.logistics_vp.security;
+
+import com.tsystems.logistics.logistics_vp.entity.AuthenticationInfo;
+import com.tsystems.logistics.logistics_vp.repository.AuthenticationInfoRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final AuthenticationInfoRepository authenticationInfoRepository;
+
+    public MyUserDetailsService(AuthenticationInfoRepository authenticationInfoRepository) {
+        this.authenticationInfoRepository = authenticationInfoRepository;
+    }
+
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AuthenticationInfo user = authenticationInfoRepository.findByUsername(username);
+        if (user==null) {
+            throw new UsernameNotFoundException("User could not be found");
+        }
+
+        List<SimpleGrantedAuthority> roles = Arrays.asList(
+                new SimpleGrantedAuthority("ROLE_" + user.getRole().toString()));
+        return new User(username, user.getPassword(), roles);
+    }
+}
